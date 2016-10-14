e6d3b3ecefd8233c68269d5f984a9fa1907f2b53
wulongwang <fbi080703@sina.com>
2016-10-14 20:22:10 +0800

    mod:add some text

--- SwiftDemo/ViewController.swift
+++ SwiftDemo/ViewController.swift
@@ -47,7 +47,50 @@ class ViewController: UIViewController {
         } else {
             print("ACCESS DENIED")
         }
+        
+        
+         //for-in 循环来遍历字符串中的 characters 属性
+        for character in "Dog!?".characters {
+            print(character)
+        }
+        
+         let exclamationMark: Character = "!"
+        
+        let catCharacters: [Character] = ["C", "a", "t", "!", "?"]
+        let catString = String(catCharacters)
+        print(catString)
+        
+        
+        //连接字符串和字符
+        var string2 = "hello world"
+        var instruction = "look over"
+        instruction += string2
+        
+        //let exclamationMark: Character = "!"
+        //welcome.append(exclamationMark)
+        
+        
+        //字符串插值 (String Interpolation)
+        let multiplier = 3
+        let message = "\(multiplier) times \\2.5 is \(Double(multiplier) * 2.5)"
+        
+        let wiseWords = "\"Imagination is more important than knowledge\" - Einstein"
+        // "Imageination is more important than knowledge" - Enistein
+        let dollarSign = "\u{24}"
+        let blackHeart = "\u{2665}"
+        let sparklingHeart = "\u{1F496}"
+        // $, Unicode 标量 U+0024 // ?, Unicode 标量 U+2665 // ?, Unicode 标量 U+1F496
+        
+        print(wiseWords+" " + dollarSign + " " + blackHeart + " " + sparklingHeart)
+        
+        //可扩展的字形群集(Extended Grapheme Clusters)
+        let eAcute: Character = "\u{E9}" // é
+        let combinedEAcute: Character = "\u{65}\u{301}" // e 后面加上 ? // eAcute 是 é, combinedEAcute 是 é
+
+        
+
     }
+    
     
     func makeIncrementer() -> ((Int) -> Int) {
         func addOne(number: Int) -> Int {
